b7d07e95a39abbc47ed4f63751d1d0ea69e293ae
Javier <javier.diosesz@gmail.com>
2025-01-31 19:17:14 -0500

    pasando a producción la BD2

--- ProyectoBackFinal/inicio/serializar.py
+++ ProyectoBackFinal/inicio/serializar.py
@@ -7,14 +7,3 @@ class ServSerializer(serializers.ModelSerializer):
         model = Servicios
         fields = '__all__'
         read_only_fields = ('ServicioNombre', 'fechaInicio', 'fechaTermino', 'estado', 'Usuario')
-
-
-# ServicioNombre = models.CharField(max_length=100)
-# detalleServicio = models.TextField(blank=True)
-# fechaInicio = models.DateTimeField(auto_now_add=True)
-# fechaTermino = models.DateField(null=True)
-# estado = models.CharField(max_length=20, default="Registrado") #En atencion, cerrado, cancelado
-# obsEstado = models.TextField(blank=True, default="Estado inicia como registrado")
-# sesiones = models.IntegerField(default=6)
-# observSesiones = models.TextField(blank=True, default="Estado inicia como registrado")
-# Usuario = models.ForeignKey(User, on_delete=models.CASCADE) # Eliminar en cascada
