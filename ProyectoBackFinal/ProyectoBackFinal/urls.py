0a14ff1dcb385e10a36dae7a33b130fe048a1b74
Javier <javier.diosesz@gmail.com>
2025-01-31 20:14:55 -0500

    quitando restframework de url3

--- ProyectoBackFinal/ProyectoBackFinal/urls.py
+++ ProyectoBackFinal/ProyectoBackFinal/urls.py
@@ -18,11 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from inicio import views
 
-from django.urls import re_path
+#from django.urls import re_path
 #from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-
+#from drf_yasg.views import get_schema_view
+#from drf_yasg import openapi
+'''
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView
@@ -41,7 +41,7 @@ schema_view = get_schema_view(
    public=True,
    permission_classes=(permissions.AllowAny,),
 )
-
+'''
 
 urlpatterns = [
     path('admin/', admin.site.urls),
