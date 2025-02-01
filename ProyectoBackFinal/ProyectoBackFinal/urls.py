f6e4d0282e977d8ae7be1e4b0e4cbe2ac873f729
Javier <javier.diosesz@gmail.com>
2025-01-31 20:12:52 -0500

    quitando restframework de url2

--- ProyectoBackFinal/ProyectoBackFinal/urls.py
+++ ProyectoBackFinal/ProyectoBackFinal/urls.py
@@ -70,4 +70,5 @@ urlpatterns = [
     #path('redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     #path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
     #path('api/docs/', SpectacularSwaggerView.as_view(url_name='api-schema'), name='api-docs'),
+    #path('api/docs/', SpectacularSwaggerView.as_view(url_name='api-schema'), name='api-docs'),
 ]
